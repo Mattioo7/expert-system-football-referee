8169b2ca8090227303168db01263082c769e5374
Mateusz Chudek <m.chudek@kaft.pl>
2024-05-19 20:55:45 +0200

    Minor order change

--- reasoning.pl
+++ reasoning.pl
@@ -27,6 +27,22 @@ make_decision(Attributes, Conditions, Decision) :-
     ;   make_decision(RemainingAttributes, NewConditions, Decision)
     ).
 
+find_optimal_attribute(Attributes, Conditions, BestAttribute) :-
+    findall(PairsCount-Attr,
+            (member(Attr, Attributes),
+             differing_pairs_count(Attr, Conditions, PairsCount)),
+            Counts),
+    keysort(Counts, SortedCounts),
+    reverse(SortedCounts, [_-BestAttribute|_]).
+
+get_distinct_values(ElementIdx, Conditions, Values) :-
+    findall(Value,
+            (football_situation(ID, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
+             meets_criteria(ID, Conditions),
+             get_answer(ID, ElementIdx, Value)),
+            AllValues),
+    sort(AllValues, Values).
+
 ask_user_for_input(BestAttribute, Values, Conditions, NewConditions) :-
     question(BestAttribute, AttributeName),
     (   BestAttribute = 9 -> format(' 9 Dynamika faulu? (w skali od 1 - lekki do 10 - bardz ostry): ')
@@ -42,11 +58,27 @@ ask_user_for_input(BestAttribute, Values, Conditions, NewConditions) :-
     ),
     append(Conditions, [NewCondition], NewConditions).
 
+get_distinct_decisions(Conditions, Decisions) :-
+    findall(Dec,
+            (football_situation(ID, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, Dec),
+             meets_criteria(ID, Conditions)),
+            AllDecisions),
+    sort(AllDecisions, Decisions).
+
 meets_criteria(_, []).
 meets_criteria(ID, [(ElementIdx, Values)|Rest]) :-
     get_answer(ID, ElementIdx, ElementValue),
     member(ElementValue, Values),
     meets_criteria(ID, Rest).
+
+differing_pairs_count(ElementIdx, Conditions, PairsCount) :-
+    findall((ID1, ID2),
+            (football_situation(ID1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
+             football_situation(ID2, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
+             ID1 < ID2,
+             differs_in_attribute_and_decision(ID1, ID2, ElementIdx, Conditions)),
+            Pairs),
+    length(Pairs, PairsCount).
 
 differs_in_attribute_and_decision(ID1, ID2, ElementIdx, Conditions) :-
     get_answer(ID1, ElementIdx, ElementValue1),
@@ -58,40 +90,8 @@ differs_in_attribute_and_decision(ID1, ID2, ElementIdx, Conditions) :-
     meets_criteria(ID1, Conditions),
     meets_criteria(ID2, Conditions).
 
-differing_pairs_count(ElementIdx, Conditions, PairsCount) :-
-    findall((ID1, ID2),
-            (football_situation(ID1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
-             football_situation(ID2, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
-             ID1 < ID2,
-             differs_in_attribute_and_decision(ID1, ID2, ElementIdx, Conditions)),
-            Pairs),
-    length(Pairs, PairsCount).
-
-get_distinct_values(ElementIdx, Conditions, Values) :-
-    findall(Value,
-            (football_situation(ID, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
-             meets_criteria(ID, Conditions),
-             get_answer(ID, ElementIdx, Value)),
-            AllValues),
-    sort(AllValues, Values).
-
-get_distinct_decisions(Conditions, Decisions) :-
-    findall(Dec,
-            (football_situation(ID, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, Dec),
-             meets_criteria(ID, Conditions)),
-            AllDecisions),
-    sort(AllDecisions, Decisions).
-
 find_matching_situations(Conditions, MatchingEntries) :-
     findall((ID, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, Dec),
             (football_situation(ID, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, Dec),
              meets_criteria(ID, Conditions)),
             MatchingEntries).
-
-find_optimal_attribute(Attributes, Conditions, BestAttribute) :-
-    findall(PairsCount-Attr,
-            (member(Attr, Attributes),
-             differing_pairs_count(Attr, Conditions, PairsCount)),
-            Counts),
-    keysort(Counts, SortedCounts),
-    reverse(SortedCounts, [_-BestAttribute|_]).
